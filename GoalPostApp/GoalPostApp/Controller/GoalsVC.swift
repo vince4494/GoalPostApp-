c599c0010c7de24366f9dcf09dbe4629fd1397a9
vince zipparro <zips55@sbcglobal.net>
2017-12-21 14:45:21 -0600

    set up first view controller and added outlets

--- GoalPostApp/GoalPostApp/Controller/GoalsVC.swift
+++ GoalPostApp/GoalPostApp/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  GoalPostApp
 //
 //  Created by vince zipparro on 12/21/17.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalButtonWasPressed(_ sender: Any)
+    {
+        print("button was pressed")
     }
-
-
+    
 }
 
